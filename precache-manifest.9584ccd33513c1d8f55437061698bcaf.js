a80fda83c43cf44982fd689f917d637463a1dda6
martinrfh <47691971+martinrfh@users.noreply.github.com>
2020-10-30 17:48:40 +0330

    Updates

---
+++ precache-manifest.9584ccd33513c1d8f55437061698bcaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6791a9293e53255025e6ac4deae49c90",
+    "url": "/movie-recommendation/index.html"
+  },
+  {
+    "revision": "15c9033e241a658af4f2",
+    "url": "/movie-recommendation/static/css/main.bdad878a.chunk.css"
+  },
+  {
+    "revision": "eea5638614f17be4bb60",
+    "url": "/movie-recommendation/static/js/2.7ece85af.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/movie-recommendation/static/js/2.7ece85af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c9033e241a658af4f2",
+    "url": "/movie-recommendation/static/js/main.6cebe91c.chunk.js"
+  },
+  {
+    "revision": "12bdc5612dd76762ac09",
+    "url": "/movie-recommendation/static/js/runtime-main.9c10aeb0.js"
+  }
+]);
